87c3076c53c2864b413c44f39f30db64f4303dea
Diego Medi <diegomr090@gmail.com>
2023-09-02 23:13:20 -0600

    0.0.1

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,10 @@ $(document).ready(function() {
         alert('Producto añadido al carrito!');
     });
 });
+
+$(document).ready(function() {
+    $('.crear').on('click', function() {
+        alert('Producto añadido a la lista');
+    });
+});
+
